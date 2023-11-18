573ef31ef9d24fda1c86d94358e5d0247778b876
FuRa1 <grevtsov.b@protonmail.com>
2023-11-18 17:57:47 +0000

    ng16

--- custom-webpack-dev.config.js
+++ custom-webpack-dev.config.js
@@ -1,9 +1,9 @@
-const ExtensionReloader = require('webpack-extension-reloader')
+const ExtReloader = require('webpack-ext-reloader');
 const config = require('./custom-webpack.config');
 
-module.exports = {...config, 
+module.exports = {...config,
     mode: 'development',
-    plugins: [new ExtensionReloader({
+    plugins: [new ExtReloader({
         reloadPage: true,
         entries: {
             background: 'background'
